3e83e8fbce7ddd1d0a4b097520fbbef5286f44e3
Victor Hugo <victorhugo156@gmail.com>
2026-03-15 14:05:01 +1100

    build file

--- src/App.tsx
+++ src/App.tsx
@@ -44,31 +44,6 @@ export function App() {
     }
   }
 
-  const downloadAndOpenEmail = () => {
-
-    // 1. Opens the browser's print dialog. If the user chooses "Save as PDF", they get their file.
-    //window.print();
-    
-    // 2. Prepares the email. 'encodeURIComponent' makes text safe for URLs (like replacing spaces with %20).
-    //const to = [
-      //"victorhugo156@gmail.com",
-      //"vi_hugo156@outlook.com"
-      // "jnovakova@printforce.com.au",
-      // "ax-admin-accounts-payable@printforce.com.au",
-    //].join(",");
-    
-    //const cc = "victorholiveira156@gmail.com";
-    
-    //const subject = encodeURIComponent(`Invoice ${invoiceData.name}`);
-    //const body = encodeURIComponent("Following my invoice");
-    
-    // 3. This opens the user's default mail app (Outlook, Gmail, Apple Mail)
-    //const mailto = `mailto:${to}?cc=${encodeURIComponent(cc)}&subject=${subject}&body=${body}`;
-    //window.location.href = mailto;
-    
-    setStep(3);
-  };
-
   return (
     <div className="min-h-screen bg-gray-100">
       <div className="no-print">
@@ -105,16 +80,6 @@ export function App() {
 
             <div className="space-y-8">
               <InvoicePreview data={invoiceData} onPdfSaved={()=> setStep(3)}/>
-
-              {/* <div className="flex flex-col md:flex-row gap-4 no-print">
-               <button 
-               onClick={downloadAndOpenEmail}
-                className="flex-1 bg-green-600 hover:bg-green-700 text-white font-bold py-4 rounded-xl shadow-lg flex items-center justify-center gap-2"
-              >
-                <i className="fas fa-envelope"></i>
-                Generate Invoice & Email HR
-              </button>
-            </div> */}
             </div>
             
           )

9df2211450f64485d7102d9433a3e287126579f9
Victor Hugo <victorhugo156@gmail.com>
2026-04-30 09:54:58 +1000

    feat:develop header layout

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -1,16 +1,24 @@
 import { FileTextIcon } from "@phosphor-icons/react";
-import { Outlet } from "react-router-dom";
+import { Separator } from "@/components/ui/separator";
+import { Link, Outlet } from "react-router-dom";
 
 export function AppHeader() {
   return (
     <div>
-      <header className="bg-purple-700 px-4 py-3 text-white no-print">
-        <div className="mx-auto flex max-w-6xl items-center gap-2">
-          <FileTextIcon size={32} />
-          <div>
-            <p className="text-2xl font-bold">Invoice Generator</p>
-            <p className="text-xs text-gray-200">Fortnightly payment processor</p>
+      <header className="h-16 w-full flex  no-print">
+        <div className="w-full mx-3 lg:mx-15 px-2 bg-purple-600 flex flex-row  items-center gap-4">
+          <div className="flex flex-row items-center gap-1">
+            <FileTextIcon size={18} />
+            <p className="whitespace-nowrap text-sm font-bold">Invoice Generator</p>
           </div>
+
+          <Separator orientation="vertical" className="h-16 bg-white" />
+
+          <nav className=" w-full flex items-center space-x-4 lg:space-x-6">
+            <Link to="/dashboard" className="text-xxs font-bold">Dashboard</Link>
+            <Link to="/dashboard" className="text-xxs font-bold">Invoices</Link>
+            <Link to="/dashboard" className="text-xxs font-bold">Dashboard</Link>
+          </nav>
         </div>
       </header>
       <Outlet />
